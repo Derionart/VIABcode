86f8877066e9bec06dbf161c3681c554b7922938
Derionart <minkov.niko@gmail.com>
2026-06-08 21:41:20 +0300

    Update and rename config.js to configexample.js

--- config.js
+++
@@ -1,5 +0,0 @@
-
-window.__APP_CONFIG__ = {
-  // Google Apps Script URL для обработки формы
-  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyduHP971R4ogORBLgKopc37-GTpaltbAQzR7zFEOKyTb2CjUaNSpKwiB42B9fEH5kqRA/exec',
-};
---
+++ configexample.js
@@ -0,0 +1,5 @@
+
+```javascript
+window.__APP_CONFIG__ = {
+  SCRIPT_URL: 'YOUR_TEST_OR_PRODUCTION_URL_HERE',
+};
